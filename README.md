a21cf01d7eb944ae66e3bf41b37a9765f0ac9ffc
Riley Bell <56055271+RileyBell0@users.noreply.github.com>
2023-08-30 15:12:12 +1000

    rust - rocket backend with tls test

--- README.md
+++ README.md
@@ -13,4 +13,27 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 OR, check out the rust website
 https://www.rust-lang.org/tools/install
 
+## Setup
+
+You've got to set up a `Rocket.toml` file in the root directory, containing
+something like this
+```
+[default]
+secret_key = "<redacted - generate with `openssl rand -base64 32`>"
+
+[default.databases.rust]
+url = "postgres://rileybell@localhost/rust"
+
+[default.tls]
+certs = "/etc/ssl/server.crt"
+key = "/etc/ssl/server.key"
+```
+
+You'll also need to make a `.env` file containing the following
+```
+# This one should be a URL, and should look something like postgresql://username@host/dbname
+# In my case, it's postgresql://rileybell@localhost/dbname". This is for sqlx's query! macro
+DATABASE_URL="<REDACTED>"
+```
+
 ## Running
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,56 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+use rocket_db_pools::{Connection, Database};
+
+#[derive(Database)]
+#[database("rust")]
+struct Logs(sqlx::PgPool);
+
+#[get("/<id>")]
+async fn dbtest(id: i32, mut db: Connection<Logs>) -> Option<String> {
+    let rows = rocket_db_pools::sqlx::query!("SELECT * FROM account where id = $1", id)
+        .fetch_all(&mut *db)
+        .await;
+
+    let rows = match rows {
+        Ok(r) => r,
+        _ => return Some("Failed".to_string()),
+    };
+
+    let mut info = Vec::new();
+
+    for row in rows {
+        info.push(format!("({},{})", row.id, row.name));
+    }
+
+    Some(info.join("\n"))
+}
+
+#[get("/")]
+fn hello() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .attach(Logs::init())
+        .mount("/rust/", routes![dbtest])
+        .mount("/rust/", routes![hello])
+}
+
+#[cfg(test)]
+mod test {
+    use super::rocket;
+    use rocket::http::Status;
+    use rocket::local::blocking::Client;
+
+    #[test]
+    fn hello_world() {
+        let client = Client::tracked(rocket()).expect("valid rocket instance");
+        let response = client.get(uri!(super::hello)).dispatch();
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(response.into_string().unwrap(), "Hello, world!");
+    }
 }
